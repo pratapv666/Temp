e3168f264e78eb518cf53769135ee1783f262ea0
pratapv666 <pratap.k@iton.com>
2017-06-14 12:25:33 +0530

    test @12:25
    test @12:25

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String a[]){
-		System.out.println("test update in Git Hello world...");
+		System.out.println("test @12:25 update in Git Hello world...");
 	}
 }
