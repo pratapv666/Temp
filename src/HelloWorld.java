0d667846cc4410a1df229c0a69eee515e73e1e92
pratapv666 <pratap.k@iton.com>
2017-06-14 12:54:51 +0530

    test commit @12:55 from local

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String a[]){
-		System.out.println("Update Hello world...");
+		System.out.println("test commit @12:55 from local to git");
 	}
 }
